aa45b72ff9c067d712df9c1e789ed93b8f176f19
Kcliffor <alexmag727>
2021-12-14 10:15:13 +0700

    nod done

--- skillbox.dart
+++ skillbox.dart
@@ -2,18 +2,21 @@ int abs(int x) => x <= 0 ? -x : x;
 
 int nod(int a, int b) {
   int res = 1;
-  if (a != 0) {
-    for (int delimeter = 1; delimeter <= abs(a); delimeter++) {
+  if (a != 0 && b != 0) {
+    for (int delimeter = abs(a) <= abs(b) ? abs(a) : abs(b);
+        delimeter > 0;
+        delimeter--) {
       if (a % delimeter == 0 && b % delimeter == 0) {
         res = delimeter;
+        break;
       }
     }
   } else {
-    res = abs(b);
+    res = abs(b) >= abs(a) ? abs(b) : abs(a);
   }
   return res;
 }
 
 void main() {
-  print(nod(0, 0));
+  print(nod(-4, 12));
 }
